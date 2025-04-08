e63d4d4d638244593d90be12092eb72d9110d413
Alisson Moraes <alissonmoraesdev@gmail.com>
2025-04-07 23:26:00 -0300

    feat: add route to delete a transaction

--- src/controllers/helpers/transaction.js
+++ src/controllers/helpers/transaction.js
@@ -1,5 +1,5 @@
 import validator from 'validator'
-import { badRequest } from './http.js'
+import { badRequest, notFound } from './http.js'
 
 export const checkIfAmountIsValid = (amount) => {
     if (typeof amount !== 'number') {
@@ -17,13 +17,17 @@ export const checkIsTypeValid = (type) => {
 }
 
 export const invalidAmountResponse = () => {
-    badRequest({
+    return badRequest({
         message: 'The amount must be a valid currency.',
     })
 }
 
 export const invalidTypeResponse = () => {
-    badRequest({
+    return badRequest({
         message: 'The type must be EARNING, EXPENSE or INVESTMENT',
     })
+}
+
+export const transactionNotFoundResponse = () => {
+    return notFound({ message: 'Transaction not found.' })
 }
--- src/factories/controllers/transaction.js
+++ src/factories/controllers/transaction.js
@@ -1,5 +1,6 @@
 import {
     CreateTransactionController,
+    DeleteTransactionController,
     GetTransactionsByUserIdController,
     UpdateTransactionController,
 } from '../../controllers/index.js'
@@ -8,9 +9,11 @@ import {
     PostgresGetUserByIdRepository,
     PostgresGetTransactionsByUserIdRepository,
     PostgresUpdateTransactionRepository,
+    PostgresDeleteTransaction,
 } from '../../repositories/postgres/index.js'
 import {
     CreateTransactionUseCase,
+    DeleteTransactionUseCase,
     GetTransactionsByUserIdUseCase,
     UpdateTransactionUseCase,
 } from '../../use-cases/index.js'
@@ -62,4 +65,18 @@ export const makeUpdateTransactionController = () => {
     )
 
     return updateTransactionController
+}
+
+export const makeDeleteTransactionController = () => {
+    const deleteTransactionRepository = new PostgresDeleteTransaction()
+
+    const deleteTransactionUseCase = new DeleteTransactionUseCase(
+        deleteTransactionRepository,
+    )
+
+    const deleteTransactionController = new DeleteTransactionController(
+        deleteTransactionUseCase,
+    )
+
+    return deleteTransactionController
 }
