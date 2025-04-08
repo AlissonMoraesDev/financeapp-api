95c0106ebb7af9806dc11ee92197943585f22c5b
Alisson Moraes <alissonmoraesdev@gmail.com>
2025-04-07 22:57:51 -0300

    feat: add route to update a transaction

--- src/repositories/postgres/transaction/update-transaction.js
+++ src/repositories/postgres/transaction/update-transaction.js
@@ -1,23 +1,23 @@
 import { PostgresHelper } from '../../../db/postgres/helper.js'
 
 export class PostgresUpdateTransactionRepository {
-    async execute(transactionId, UpdateTransactionParams) {
+    async execute(transactionId, updateTransactionParams) {
         const updateFields = []
         const updateValues = []
 
-        Object.keys(UpdateTransactionParams).forEach((key) => {
+        Object.keys(updateTransactionParams).forEach((key) => {
             updateFields.push(`${key} = $${updateValues.length + 1}`)
-            updateValues.push(UpdateTransactionParams[key])
+            updateValues.push(updateTransactionParams[key])
         })
 
         updateValues.push(transactionId)
 
         const updateQuery = `
-      UPDATE transactions
-      SET ${updateFields.join(', ')}
-      WHERE id = $${updateValues.length}
-      RETURNING *
-    `
+            UPDATE transactions
+            SET ${updateFields.join(', ')}
+            WHERE id = $${updateValues.length}
+            RETURNING *
+        `
 
         const updateTransaction = await PostgresHelper.query(
             updateQuery,
